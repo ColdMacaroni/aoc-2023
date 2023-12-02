8136b7babddc33bdb199f2ff09e55f31d948b5a3
sofa <babalark@proton.me>
2023-12-02 18:47:40 +1300

    Do part2

--- day2/README.md
+++ day2/README.md
@@ -3,3 +3,8 @@
 ## Part 1
 
 This part 1 doesn't look that bad. I figure I should get bash out of the way before the harder problems.
+Yeah that was pretty easy!!
+
+## Part 2
+
+Even easier tbh!! Surprisingly, Bash was a good pick.
---
+++ day2/part2.bash
@@ -0,0 +1,17 @@
+# Elf wants
+power_sum=0
+
+parse() {
+    id="$(echo "$inp" | cut -d' ' -f2 | tr -d :)"
+    declare -A mins
+    for c in red blue green; do
+        min_num="$(echo "$inp" | grep -o "[0-9]\+ $c" | cut -f1 -d' ' | sort -r -n | head -1)"
+        mins[$c]=$min_num
+    done
+
+    power_sum=$((power_sum + mins[red] * mins[blue] * mins[green]))
+}
+
+while read -r inp; do parse "$inp"; done
+
+echo "$power_sum"
